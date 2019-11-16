ba12a2217495c6faefcf9fde364dc185dddc6782
Bob Kozdemba <bkozdemba@gmail.com>
2019-11-15 20:42:24 -0600

    updated

--- c2/functions.py
+++ c2/functions.py
@@ -1,3 +1,13 @@
 #
 # Week 3 - Functions
 #
+def my_function(): 
+	"""Demonstrate docstrings and does nothing really."""
+
+	return None
+
+print("Using __doc__:")
+print(my_function.__doc__) 
+
+print("Using help:")
+help(my_function) 
