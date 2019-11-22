b5d139def163c5ee0a47fe42cea4a1ccc8428724
Bob Kozdemba <bkozdemba@gmail.com>
2019-11-22 12:34:30 -0600

    initial

---
+++ c3/listComprehension.py
@@ -0,0 +1,19 @@
+#
+# Extract all the names.
+#
+tester = {'info': [{"name": "Lauren", 'class standing': 'Junior', 'major': "Information Science"},{'name': 'Ayo', 'class standing': "Bachelor's", 'major': 'Information Science'}, {'name': 'Kathryn', 'class standing': 'Senior', 'major': 'Sociology'}, {'name': 'Nick', 'class standing': 'Junior', 'major': 'Computer Science'}, {'name': 'Gladys', 'class standing': 'Sophomore', 'major': 'History'}, {'name': 'Adam', 'major': 'Violin Performance', 'class standing': 'Senior'}]}
+for n in tester['info']:
+    print(n['name'])
+compri = [n['name'] for n in tester['info']]
+
+#
+# Extract numbers > 10
+#
+L = [12, 34, 21, 4, 6, 9, 42]
+lst = []
+for x in L:
+    if x > 10:
+        lst.append(x)
+print(lst)
+
+lst2 = [num for num in L if num > 10]
