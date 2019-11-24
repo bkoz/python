0e82ca628f183d36cc39d4410e6923e38ce8c23b
Bob Kozdemba <bkozdemba@gmail.com>
2019-11-24 14:14:05 -0600

    updated

--- c3/c3project-cache.py
+++ c3/c3project-cache.py
@@ -10,7 +10,7 @@ requests_cache.install_cache('requests_cache', expire_after=None)
 #
 # Question 3
 #
-import requests_with_caching
+# import requests_with_caching
 
 def get_movies_from_tastedive(movie):
     baseurl = "https://tastedive.com/api/similar"
@@ -46,5 +46,26 @@ def get_related_titles(movieList):
 # some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
 print(get_related_titles(["Black Panther", "Captain Marvel"]))
 # get_related_titles([])
+
+
+def get_movie_data(title):
+#http://www.omdbapi.com/?apikey=68415c82&type=movie&t=John+Wick
+    baseurl = "http://www.omdbapi.com"
+    params_diction = {} # Set up an empty dictionary for query parameters
+    params_diction['t'] = title
+    # The type=movies is not consistent with the api docs.
+    # It specifies singular, (i.e. type=movie)
+    params_diction['r'] = 'json'
+    params_diction['apikey'] = '68415c82'
+    resp = requests.get(baseurl, params=params_diction)    
+    #print(resp.url)
+    #print(resp.text)
+    return resp.json()  
+
+    
+# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
+print('-----------------------------------------------\n')
+print(get_movie_data("Venom"))
+# get_movie_data("Baby Mama")
 
 
