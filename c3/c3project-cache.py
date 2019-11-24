0dd6e4d4d5cefb8e860035993d11f8989362bbbb
Bob Kozdemba <bkozdemba@gmail.com>
2019-11-24 11:33:53 -0600

    updated

--- c3/c3project-cache.py
+++ c3/c3project-cache.py
@@ -1,47 +1,10 @@
 #
-# Question 1
+# Questions 1 through 3.
 #
-import requests_with_caching
+import requests
+import requests_cache
 
-def get_movies_from_tastedive(movie):
-    baseurl = "https://tastedive.com/api/similar"
-    params_diction = {} # Set up an empty dictionary for query parameters
-    params_diction["q"] = movie
-    # The type=movies is not consistent with the api docs.
-    # It specifies singular, (i.e. type=movie)
-    params_diction["type"] = "movies"
-    params_diction["limit"] = 5 # get at most 5 results
-    resp = requests_with_caching.get(baseurl, params=params_diction)    
-    return resp.json()  
-
-get_movies_from_tastedive("Bridesmaids")
-get_movies_from_tastedive("Black Panther")
-
-#
-# Question 2
-#
-import requests_with_caching
-
-def get_movies_from_tastedive(movie):
-    baseurl = "https://tastedive.com/api/similar"
-    params_diction = {} # Set up an empty dictionary for query parameters
-    params_diction["q"] = movie
-    # The type=movies is not consistent with the api docs.
-    # It specifies singular, (i.e. type=movie)
-    params_diction["type"] = "movies"
-    params_diction["limit"] = 5 # get at most 5 results
-    resp = requests_with_caching.get(baseurl, params=params_diction)    
-    return resp.json()  
-
-def extract_movie_titles(dict):
-    movies = []
-    for movie in dict['Similar']['Results']:
-        movies.append(movie['Name'])
-    return movies
-
-# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
-# extract_movie_titles(get_movies_from_tastedive("Tony Bennett")) 
-# extract_movie_titles(get_movies_from_tastedive("Black Panther"))
+requests_cache.install_cache('requests_cache')
 
 #
 # Question 3
@@ -56,7 +19,7 @@ def get_movies_from_tastedive(movie):
     # It specifies singular, (i.e. type=movie)
     params_diction["type"] = "movies"
     params_diction["limit"] = 5 # get at most 5 results
-    resp = requests_with_caching.get(baseurl, params=params_diction)    
+    resp = requests.get(baseurl, params=params_diction)    
     return resp.json()  
 
 def extract_movie_titles(dict):
@@ -80,7 +43,7 @@ def get_related_titles(movieList):
 
 
 # some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
-# get_related_titles(["Black Panther", "Captain Marvel"])
+print(get_related_titles(["Black Panther", "Captain Marvel"]))
 # get_related_titles([])
 
 
