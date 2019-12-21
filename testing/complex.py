643f371e925a3c2eb7a4d29c9f6c9ceed2dc3efe
Bob Kozdemba <bkozdemba@gmail.com>
2019-12-21 12:59:01 -0600

    updated

--- testing/complex.py
+++ testing/complex.py
@@ -4,8 +4,12 @@
 import cmath
 import random
 
-a = complex('(1+2j)')
-b = complex(1, 2)
+a = complex('(2+j)')
+b = complex(3, 1)
+z = complex()
 
-print(a*b)
-print(random.randrange(0, 15))
+print('z = {}, z^2 + 5 = {}'.format(z, (z**2) + 5))
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} * {} = {}'.format(a, b, a * b))
+
+print('Random number between 0 and 15 = ', random.randrange(0, 15))
