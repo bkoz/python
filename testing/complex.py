144a8e3f51b444c2b1490b75e27d93a6e4b5a819
Bob Kozdemba <bkozdemba@gmail.com>
2019-12-21 13:05:19 -0600

    updated

--- testing/complex.py
+++ testing/complex.py
@@ -8,8 +8,9 @@ a = complex('(2+j)')
 b = complex(3, 1)
 z = complex()
 
-print('z = {}, z^2 + 5 = {}'.format(z, (z**2) + 5))
 print('{} + {} = {}'.format(a, b, a + b))
+# (2+i)(3+i) = 5+5i.
 print('{} * {} = {}'.format(a, b, a * b))
+print('z = {}, z^2 + 5 = {}'.format(z, (z**2) + 5))
 
 print('Random number between 0 and 15 = ', random.randrange(0, 15))
