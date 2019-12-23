2b45738015249444e97869e75e81b667180b5be4
Bob Kozdemba <bkozdemba@gmail.com>
2019-12-23 15:43:30 -0500

    added colormap

--- testing/imageDisplay.py
+++ testing/imageDisplay.py
@@ -4,15 +4,23 @@ import numpy as np
 
 #img = Image.open('felix-kozdemba.jpg')
 
-size = (64, 64, 3)
+def buildColorMap():
+    i = 0
+    cmap = {}
+    for r in range(8):
+        for g in range(8):
+            for b in range(4):
+                cmap[i] = (int(r/7*255), int(g/7*255), int(b/3*255))
+                i += 1
+    return cmap
+
+size = (256, 256, 3)
+cm = buildColorMap()
 i = np.zeros(size, dtype=np.int8)
 for y in range(size[1]):
     for x in range(size[0]):
-        if y % 2:
-            i[y][x][0] = 255
-            i[y][x][1] = 0
-            i[y][x][2] = 0
-
+            i[y][x] = cm[y]
+            
 # i[0][31][0] = 255
 # i[0][31][1] = 255
 # i[0][31][2] = 255
