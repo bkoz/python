3ef3add5625384d086bf4c4288379631d243d819
Bob Kozdemba <bkozdemba@gmail.com>
2019-12-29 19:53:37 -0500

    updated

--- mandelbrot/mandelbrotComplex.py
+++ mandelbrot/mandelbrotComplex.py
@@ -10,7 +10,7 @@ def zeros(rows, cols):
 
 
 d = 100 # Pixel density 
-n = 50 # Maximum number of iterations
+n = 32 # Maximum number of iterations
 maxN = -1
 
 x = linspace(-2.5, 1.5, 4 * d + 1)
@@ -21,13 +21,13 @@ T = zeros(len(y), len(x))
 for i, b in enumerate(y):
     for j, a in enumerate(x):
         # Mandelbrot set: c(a, b), z(0, 0)
-        # c = complex(a, b)
-        # z = complex(0, 0)
+        c = complex(a, b)
+        z = complex(0, 0)
         
         # Julia Set: Constant c, z(a,b)
-        #c = complex('(0.285+0.01j)')
-        c = complex('(-0.8+0.156j)')
-        z = complex(a, b)
+        # c = complex('(0.285+0.01j)')
+        # c = complex('(-0.8+0.156j)')
+        # z = complex(a, b)
         for k in range(n):
             z = z * z + c
             if complex.__abs__(z) >= 2:
@@ -36,7 +36,7 @@ for i, b in enumerate(y):
                     maxN = k
                 break
 
-plt.imshow(T, cmap=plt.cm.jet)
+plt.imshow(T, cmap=plt.cm.twilight_shifted)
 plt.show()
 #plt.savefig('mandelbrot.png', dpi=200)
 print('maxN = {}'.format(maxN))
